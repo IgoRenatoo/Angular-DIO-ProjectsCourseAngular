e6f64c18f3aa1677776c0516fbc76c488355ea2c
IgoRenatoo <igor.renato.oliveira@gmail.com>
2025-03-09 12:45:05 -0300

    refactor: alterado estrutura 'content'
    - Substituído nome da estrutura para 'Home'
    - Melhoria e padronização do CSS

--- 01 - ToDoList/src/app/main/main.component.ts
+++ 01 - ToDoList/src/app/main/main.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { ContentComponent } from './content/content.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @Component({
   selector: 'app-main',
-  imports: [ContentComponent],
+  imports: [HomeComponent, AboutComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
