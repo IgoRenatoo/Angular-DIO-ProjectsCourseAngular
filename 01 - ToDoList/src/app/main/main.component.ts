21274c78dd212df2935f1d818bb19332581f902d
IgoRenatoo <igor.renato.oliveira@gmail.com>
2025-03-09 13:15:05 -0300

    feat: estrutura 'login'

--- 01 - ToDoList/src/app/main/main.component.ts
+++ 01 - ToDoList/src/app/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-main',
-  imports: [HomeComponent, AboutComponent],
+  imports: [HomeComponent, AboutComponent, LoginComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
