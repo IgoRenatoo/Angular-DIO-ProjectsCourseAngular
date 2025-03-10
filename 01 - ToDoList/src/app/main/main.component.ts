b023aa00e35b268ca5a7a2226d0473c8637882c4
IgoRenatoo <igor.renato.oliveira@gmail.com>
2025-03-10 03:46:08 -0300

    feat: estrutura 'task'

--- 01 - ToDoList/src/app/main/main.component.ts
+++ 01 - ToDoList/src/app/main/main.component.ts
@@ -3,10 +3,11 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-main',
-  imports: [HomeComponent, AboutComponent, LoginComponent, SignupComponent],
+  imports: [HomeComponent, AboutComponent, LoginComponent, SignupComponent, TaskComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
