6039a3fa03d1d0f840302a16951b718722bae19e
Igor Oliveira <igor.renato.oliveira@gmail.com>
2025-03-21 21:32:59 -0300

    feat: autenticação com route guard no front end
    - Pendente autenticação via JWT no backend para fins didático.

--- 01 - ToDoList/src/main.ts
+++ 01 - ToDoList/src/main.ts
@@ -7,13 +7,15 @@ import { AboutComponent } from './app/main/about/about.component'
 import { LoginComponent } from './app/main/login/login.component'
 import { SignupComponent } from './app/main/signup/signup.component'
 import { TaskComponent } from './app/main/task/task.component'
+import { authGuard } from './app/guard/auth.guard'
 
 const routes: Route[] = [
   { path: '', component: HomeComponent },
-  { path: 'tarefas', component: TaskComponent },
+  { path: 'tarefas', component: TaskComponent, canActivate: [authGuard] },
   { path: 'sobre', component: AboutComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'cadastro', component: SignupComponent }
+  { path: 'cadastro', component: SignupComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 ]
 
 bootstrapApplication(MainComponent, { providers: [provideRouter(routes), provideHttpClient()] }).catch(err => console.error(err))
